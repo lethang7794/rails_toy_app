fc841ad16826d556078535b9cc1139e369176426
lethang7794 <lethang7794@gmail.com>
2020-05-06 19:07:32 +0700

    Ensure name, email and content can't be blank

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
     belongs_to :user
 
-    validates :content, length: { maximum: 140 }
+    validates :content, length: { maximum: 140 },
+                        presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :microposts
+
+    validates :name, presence: true
+    validates :email, presence: true
 end
